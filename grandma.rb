87a9d6496bad7229c3e2033b136a1f2633115254
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-10 18:00:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase = "I LOVE YOU GRANDMA!")
   return "HUH?! SPEAK UP, SONNY!"
 
   if phrase == "I LOVE YOU GRANDMA!"
-    puts "HUH?! SPEAK UP, SONNY!"
+    puts "I LOVE YOU TOO PUMPKIN!"
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
