7e76dfcab1480e3cdcbd648d238b9840d6611308
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-10 17:56:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma
   speak_to_grandma = "I LOVE YOU GRANDMA!"
-  if speak_to_grandma
-    puts "I LOVE YOU TOO PUMPKIN!"
+  return "HUH?! SPEAK UP, SONNY!"
+  end
 end
 
 # Whatever you say to grandma, she should respond with
