8e1f137ca9602b6fa44f6df8a7ab0c42c389c919
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-10 18:03:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 def speak_to_grandma(phrase)
  if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  else phrase == "Hi, nana, how a re you?"
+    return "HUH?! SPEAK UP, SONNY!"
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
